9d784e0f87eadae31837b79d3ae84fc68e1d09f3
main <mokamharoldroger@gmail.com>
2025-07-14 22:06:04 +0100

    Projet initial

---
+++ src/main/java/com/haroldmokam/ma_biblio/MaBiblioApplication.java
@@ -0,0 +1,13 @@
+package com.haroldmokam.ma_biblio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaBiblioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaBiblioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haroldmokam/ma_biblio/MaBiblioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haroldmokam.ma_biblio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaBiblioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
