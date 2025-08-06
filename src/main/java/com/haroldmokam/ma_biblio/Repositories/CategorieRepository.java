2dd5db76b74798aad27aba5e582b0cedb70143d5
main <mokamharoldroger@gmail.com>
2025-08-06 18:40:18 +0100

    Creation des entites CategoryRepository et CategorieService pour la gestion de la logiqure metier des categories dans la base de donnees

---
+++ src/main/java/com/haroldmokam/ma_biblio/Repositories/CategorieRepository.java
@@ -0,0 +1,19 @@
+package com.haroldmokam.ma_biblio.Repositories;
+
+import com.haroldmokam.ma_biblio.entites.Categorie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CategorieRepository extends JpaRepository<Categorie, Integer> {
+    public Optional<Categorie> findById(int id);
+    public Optional<Categorie> findByLibelle(String libelle);
+
+    // Cette methode permet de rechercer une liste de Categorie en fonction du libelle en ignorant les Majuscules et Minuscules.
+    public List<Categorie> findByLibelleContainingIgnoreCase(String libelle);
+
+    public List<Categorie> findAll();
+    public void deleteById(int id);
+    public void deleteByLibelle(String libelle);
+}
---
+++ src/main/java/com/haroldmokam/ma_biblio/services/CategorieService.java
@@ -0,0 +1,73 @@
+package com.haroldmokam.ma_biblio.services;
+
+import com.haroldmokam.ma_biblio.Repositories.CategorieRepository;
+import com.haroldmokam.ma_biblio.entites.Categorie;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @Author : Mokam Harold
+ *
+ */
+@Service
+@AllArgsConstructor
+public class CategorieService {
+
+    //Injection des dependances
+    private CategorieRepository categorieRepository;
+
+
+    public void creerCategorie(Categorie categorie) {
+        Optional<Categorie> categorieDansBD = categorieRepository.findByLibelle(categorie.getLibelle());
+        if(categorieDansBD.isEmpty()) {
+            categorieRepository.save(categorie);
+        }
+        else{
+            throw new RuntimeException("L'element existe deja dans la base de donnees");
+        }
+
+    }
+
+    public void modifierCategorie(Categorie categorie) {
+        Optional<Categorie> categorieDansBD = categorieRepository.findByLibelle(categorie.getLibelle());
+        if(categorieDansBD.isPresent()) {
+            categorieRepository.save(categorie);
+        }
+        else{
+            throw new RuntimeException("L'element n'existe pas dans la base de donnees.");
+        }
+    }
+
+    public void supprimerCategorie(Categorie categorie) {
+        Optional<Categorie> categorieDansBD = categorieRepository.findByLibelle(categorie.getLibelle());
+        if(categorieDansBD.isPresent()) {
+            categorieRepository.delete(categorie);
+        }
+        else{
+            throw new RuntimeException("L'element n'existe pas dans la base de donnees ou elle a deja ete supprimmee.");
+        }
+    }
+
+    public List<Categorie> rechercherCategorie( String libelle) {
+        List<Categorie> categorieDansBD = categorieRepository.findByLibelleContainingIgnoreCase(libelle);
+        if(categorieDansBD.isEmpty()) {
+            throw new RuntimeException("Aucun element trouve dans la base de donnees.");
+        }
+        return categorieDansBD;
+    }
+
+    public List<Categorie> rechercherAllCategories() {
+        return categorieRepository.findAll();
+    }
+
+    public Optional<Categorie> rechercherCategorieParId(int id) {
+       Optional <Categorie> categorieDansBD = categorieRepository.findById(id);
+       if(categorieDansBD.isEmpty()) {
+           throw new RuntimeException("Cette categorie n'existe pas ou a deja ete supprimee.");
+       }
+       return categorieDansBD;
+    }
+}
