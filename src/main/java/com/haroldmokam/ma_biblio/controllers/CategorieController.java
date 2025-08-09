67cc9e1b4906344b32a1172b3c2ce1a1ceccee88
main <mokamharoldroger@gmail.com>
2025-08-09 08:12:41 +0100

    Creation des controllers

---
+++ src/main/java/com/haroldmokam/ma_biblio/controllers/CategorieController.java
@@ -0,0 +1,46 @@
+package com.haroldmokam.ma_biblio.controllers;
+
+import com.haroldmokam.ma_biblio.entites.Categorie;
+import com.haroldmokam.ma_biblio.services.CategorieService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@AllArgsConstructor
+@RequestMapping
+public class CategorieController {
+    private final CategorieService categorieService;
+
+    @PostMapping(path = "/ajouter", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void ajouterCategorie(@RequestBody Categorie categorie) {
+        log.info(categorie.getLibelle()+ " a bien ete cree et enregistre dans la base de donnees");
+        categorieService.creerCategorie(categorie);
+    }
+
+    @PutMapping(path = "/modifier/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void modifierCategorie(@PathVariable int id, @RequestBody Categorie categorie) {
+         Categorie categorieDansBD = categorieService.rechercherCategorieParId(id);
+         String ancienLibelle = categorieDansBD.getLibelle();
+         categorieDansBD.setLibelle(categorie.getLibelle());
+        categorieService.creerCategorie(categorieDansBD);
+        log.info("La categorie est quitte de %s a %s", ancienLibelle, categorieDansBD.getLibelle());
+    }
+
+    @DeleteMapping(path = "/supprimer/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void supprimerCategorie(@PathVariable int id) {
+        Categorie categorieDansBD = categorieService.rechercherCategorieParId(id);
+        categorieService.supprimerCategorie(categorieDansBD);
+        log.info("La categorie %s a ete supprimee de la base de donnees", categorieDansBD.getLibelle());
+    }
+
+    @GetMapping(path = "/Liste")
+    public List<Categorie> listerCategorie() {
+        log.info("Liste des categories");
+        return categorieService.rechercherAllCategories();
+    }
+}
---
+++ src/main/java/com/haroldmokam/ma_biblio/controllers/EmpruntController.java
@@ -0,0 +1,4 @@
+package com.haroldmokam.ma_biblio.controllers;
+
+public class EmpruntController {
+}
